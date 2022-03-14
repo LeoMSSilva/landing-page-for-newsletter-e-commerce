82f7f41ea9bc51f2903dc7c07fad3d2ab01d1cbb
LeoMSSilva <l.eom1994@hotmail.com>
2022-03-14 19:24:19 -0300

    Refactor: clean code in names and functions

--- js/script.js
+++ js/script.js
@@ -2,59 +2,61 @@ const display = document.querySelector('#display');
 const form = document.querySelector('#form');
 const erro = document.querySelector('#error');
 
-const printCount = (days, hours, minutes, seconds) => {
+const formatePrintCounter = (days, hours, minutes, seconds) => {
 	hours = hours < 10 ? '0' + hours : hours;
 	minutes = minutes < 10 ? '0' + minutes : minutes;
 	seconds = seconds < 10 ? '0' + seconds : seconds;
-	display.innerHTML = `${days} : ${hours} : ${minutes} : ${seconds}`;
+	display.innerHTML = `${days}:${hours}:${minutes}:${seconds}`;
 };
 
-(() => {
+const bootstrap = () => {
 	const second = 1000;
 	const minute = second * 60;
 	const hour = minute * 60;
 	const day = hour * 24;
-	let days, hours, minutes, seconds;
-	let finalTimer = 'Jul 18, 2021 23:59:00';
-	let countDown = new Date(finalTimer).getTime();
+	const finalTimer = 'Jul 11, 2024 23:59:00';
+	const countDown = new Date(finalTimer).getTime();
 	setInterval(() => {
-		let now = new Date().getTime();
+		const now = new Date().getTime();
 		const distance = countDown - now;
-		days = Math.floor(distance / day);
-		hours = Math.floor((distance % day) / hour);
-		minutes = Math.floor((distance % hour) / minute);
-		seconds = Math.floor((distance % minute) / second);
-		printCount(days, hours, minutes, seconds);
+		const currentDays = Math.floor(distance / day);
+		const currentHours = Math.floor((distance % day) / hour);
+		const currentMinutes = Math.floor((distance % hour) / minute);
+		const currentSeconds = Math.floor((distance % minute) / second);
+		formatePrintCounter(
+			currentDays,
+			currentHours,
+			currentMinutes,
+			currentSeconds,
+		);
 	}, 1000);
-})();
+};
+bootstrap();
 
-function validation(name, email) {
+const validation = (name, email) => {
 	if (name == '') return 'Por favor, preencha o nome antes de enviar!';
-	if (email == '') return 'Por favor, preencha o e-mail antes de enviar!';
-	return '';
-}
+	return email == '' ? 'Por favor, preencha o e-mail antes de enviar!' : '';
+};
 
-function load() {
-	let content = document.querySelector('#content');
-	let carregando = `<p>Processando...</p>`;
-	let pronto = `<p>Cadastrado com sucesso!</p>`;
-	content.innerHTML = carregando;
-	setTimeout(() => {
-		content.innerHTML = pronto;
-	}, 1000);
-}
+const loadingRegistred = () => {
+	const contentElement = document.querySelector('#content');
+	const loading = `<p>Processando...</p>`;
+	const registeredUser = `<p>Cadastrado com sucesso!</p>`;
+	contentElement.innerHTML = loading;
+	setTimeout(() => (contentElement.innerHTML = registeredUser), 1000);
+};
 
-function saveData(name, email) {
-	let data = { name, email };
-	let convertToString = JSON.stringify(data);
-	localStorage.setItem('leads', convertToString);
-	load();
-}
+const saveData = (name, email) => {
+	const data = { name, email };
+	const dataConvertedToString = JSON.stringify(data);
+	localStorage.setItem('leads', dataConvertedToString);
+	loadingRegistred();
+};
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
-	let name = form.querySelector('#nome').value;
-	let email = document.querySelector('#email').value;
+	const name = form.querySelector('#nome').value;
+	const email = form.querySelector('#email').value;
 	erro.innerHTML = `${validation(name, email)}`;
 	if (erro.innerHTML == '') saveData(name, email);
 });
